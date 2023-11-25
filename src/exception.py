aa24c59ad2c7d03a0dff21d7b07d17eedae8e302
harshal102 <harshal10200@gmail.com>
2023-11-25 16:27:17 +0530

    eda and problem satement

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-import logging
+from src.logger import logging
 
 #error message info
 def error_message_detail(error,error_detail:sys):
--- src/logger.py
+++ src/logger.py
@@ -15,6 +15,13 @@ logging.basicConfig(
     level=logging.INFO,
 )
 
+if __name__=="__main__":
+    try:
+        a=1/0
+    except Exception as e:
+        logging.info("divided by zero")
+        raise CustomException(e,sys)
+
 
 """import logging
 import os 
